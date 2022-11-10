0c7a4fb8e145688a2464b44d85f2584caa61d74d
Chatrughan Prasad <chatrughan.prasad@outlook.com>
2022-11-10 20:01:46 +0530

    Create Kubernetes-commands.sh

---
+++ Kubernetes-commands.sh
@@ -0,0 +1,124 @@
+KubeCtl Commnads:
+
+
+ kubectl -help
+ kubectl --help
+ kubectl top
+ kubectl top node
+ kubectl create deployment nginx-server --image=nginx
+ kubectl get deployment
+ kubectl get pod
+ kubectl get relicaset
+ kubectl get replicaset
+ kubectl describe deployment nginx-server
+ kubectl describe pod
+ kubectl edit depoloyment
+ kubectl edit deployment
+ kubectl get pods
+ kubectl edit deployment
+ kubectl get pods
+ kubectl logs nginx-server-6b5584dbf8-99sb4
+ kubectl exec -t nginx-server-6b5584dbf8-99sb4 --bin/bash
+ kubectl exec -it nginx-server-6b5584dbf8-99sb4 --bin/bash
+ kubectl exec -it nginx-server-6b5584dbf8-99sb4 -- bin/bash
+ kubectl delete deployment nginx-server
+ kubectl get pld
+ kubectl get pod
+ kubectl get deployment
+ kubectl get service
+ history
+ docker pull dailycodebuffer/dockerpublish
+ kubectl create deployment nginx-server --image=dailycodebuffer/dockerpublish
+ kubectl get pod
+ kubectl get replicaset
+ minikube dashboard
+ kubectl create deployment nginx-server --image=nginx
+ kubectl create deployment apache-server --image=nginx
+ kubectl get deployment
+ kubectl get pod
+ kubectl delete deployment nginx-server
+ minikube dashboard
+ kubectl get deployment
+ kubectl get service
+ kubectl get service apache-server
+ kubectl get service bubernetes
+ kubectl get service kubernetes
+ kubectl get deployment -o yaml
+ minikube service apache-server
+ kubectl get service
+ minikube kubernetes
+ minikube service kubernetes
+ kubectl get namespaces
+ ls
+ ls -lah
+ kubectl api-resources --namespaced=true
+ kubectl api-resources --namespaced=false
+ kk
+ kubectl get namespaces
+ kubectl create namespace my-namespace
+ kubectl get namespaces
+ kubectl apply -f .\deploy.yml --namespace=my-namespace
+ kubectl get deployment
+ kubectl create deployment webapache-server --image=httpd --namespace=my-namespace
+ kubectl get deployment
+ kubectl get deployment -n mynamespace
+ kubectl get deployment -n my-namespace
+ history
+ kubectl get service
+ kubectl create deployment newapache-server --image=httpd
+ kubectl get deployment
+ minikube addons list
+ minikube addons enable ingress
+ minikube addons list
+ kubectl get all
+ kubectl get ingress
+ kubectl get ingresses
+ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/aws/deplo...
+ cd .\Documents\
+ kubectl get all
+ kubectl apply -f ./
+ kubectl get ingress
+ ls
+ cd .\ingress\
+ kubectl apply -f ./
+ kubectl get ingress
+ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/aws/deplo...
+ kubectl get ingress
+ kubectl apply -f .\ingressresourcebackend.yml
+ kubectl describe ingress ingress-resource-backend
+ kubectl apply -f .\ingresswildcard.yml
+ kubectl apply -f .\externallb.yml
+ kubectl apply -f .\defaultingress.yml
+ kubectl apply -f .\testingress.yml
+ kubectl get ingress test-ingress
+ kubectl apply -f .\simplexaple.yml
+ kubectl describe ingress simple-fanout-example
+ kubectl apply -f .\virtualhost.yml
+ kubectl apply -f .\notheirdhost.yml
+ kubectl get ingress
+ kubectl get ingress
+ kubectl apply -f .\
+ minikube addons list
+ minikube addons enable efk
+ minikube addons list
+ minikube efk
+ minikube --help
+ cd..
+ dir
+ cd .\Kubernetes-Tutorial-main\
+ ls
+ cd .\Ingress-Tutorial\
+ ls
+ kubectl apply -f .\
+ cd ..
+ ls
+ cd  '.\Kubernetes Volumes\'
+ ls
+ kubectl apply -f .\
+ kubectl get al
+ kubectl get all
+ kubectl get
+ kubectl get -h
+ kubectl apply -f .\web.yml
+ kubectl get pods -w -l app=nginxkubectl get pods -w -l app=nginx
+ kubectl get pods -w -l app=nginx
